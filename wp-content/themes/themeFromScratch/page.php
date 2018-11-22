e3cf1c69e893f5f43af9372e62b3019f3927f86a
Franklin <franklin.fargoj@neosofttech.com>
2018-11-22 12:20:06 +0530

    Removed JS content

--- wp-content/themes/themeFromScratch/page.php
+++ wp-content/themes/themeFromScratch/page.php
@@ -1,7 +1,6 @@
 <?php get_header(); ?>
 
-<div id="owl-demo" class="carousel slide" data-ride="carousel" xmlns="http://www.w3.org/1999/html"
-     xmlns="http://www.w3.org/1999/html">
+<div id="owl-demo" class="carousel slide" data-ride="carousel" xmlns="http://www.w3.org/1999/html">
         <?php
         $query = new WP_Query( array( 'post_type' => 'carosal', 'paged' => $paged ) );
         if ( $query->have_posts() ) : ?>
@@ -39,7 +38,7 @@
             }
             ?>
 
-            <?php
+            <?php if( get_query_var('pagename') == ''){ //displays the post only on home page
             // The Query
             $query = new WP_Query(array('post_type' => 'my-product'));
             query_posts( $query );
@@ -55,21 +54,26 @@
                 ?>
                 </div>
 
-                <div>
-                      <?php the_title(); ?>
-                INR.  <?php echo get_post_meta($post->ID, 'my_product_price_value_key', true); ?>
-                Qty   <INPUT id="txtNumber"  onkeypress="return isNumberKey(event)" type="text" name="txtNumber" style="width: 50px;">
-                      <button>Add to cart</button>
-                </div>
+<!--                     <div> --><?php //echo $post->ID;?><!-- </div>-->
+                     <div> <?php the_title(); ?> </div>
+                Rs.  <span id="prod_<?php echo $post->ID; ?>"><?php echo get_post_meta($post->ID, 'my_product_price_value_key', true); ?></span>
+                Qty  <INPUT id="txtNumber<?php echo $post->ID; ?>" onkeypress="return isNumberKey(event)" type="number" min="1" value='1' style="width: 50px;">
+
+                </br>
+                <a id="add_to_cart" href="javascript:void(0);" class="btn btn-default add-to-cart" data-value="<?php echo $post->ID; ?>">
+                       <button>Add to cart</button>
+                </a>
+
+                <button>Wishlist</button>
                 </br>
 
-            <?php
+                <?php
             endwhile;
             // Reset Query
             wp_reset_query();
+            }
             ?>
         </div>
-
 
         <div class="col-lg-2">
             <?php get_sidebar('right'); ?>
@@ -81,35 +85,3 @@
 
 <?php get_footer(); ?>
 
-<script>
-    $(document).ready(function() {
-        $("#owl-demo").owlCarousel({
-            navigation : true, // Show next and prev buttons
-            autoPlay: 3000,
-            slideSpeed : 50,
-            paginationSpeed : 50,
-            items : 1,
-            responsive : {
-                480 : { items : 1  }, // from zero to 480 screen width 4 items
-                768 : { items : 2  }, // from 480 screen widthto 768 6 items
-                1024 : { items : 3   // from 768 screen width to 1024 8 items
-                }
-            },
-            itemsDesktop : false,
-            itemsDesktopSmall : false,
-            itemsTablet: false,
-            itemsMobile : false
-        });
-    });
-
-
-    function isNumberKey(evt)
-    {
-        var charCode = (evt.which) ? evt.which : evt.keyCode;
-        if (charCode != 46 && charCode > 31
-            && (charCode < 48 || charCode > 57))
-            return false;
-        return true;
-    }
-
-</script>

b6086f4023d9d9a395b9f4c5bec0e21b00394bae
Franklin <franklin.fargoj@neosofttech.com>
2018-12-04 13:14:48 +0530

    Modification in confirmation template

--- wp-content/themes/themeFromScratch/confirmation-template.php
+++ wp-content/themes/themeFromScratch/confirmation-template.php
@@ -12,62 +12,59 @@ get_header();
         <h3>Order Confirmation<h3>
         <p><?php echo 'Hello, ' . $current_user->display_name . "\n";  ?></p>
 
-        <table class="table">
-            <thead>
-            <tr>
-                <th>Product Name</th>
-                <th>Image</th>
-                <th>Qty.</th>
-            </tr>
-            </thead>
-            <tbody>
-            <?php
-            if(!empty($_SESSION['cart_items'])){
-                foreach ($_SESSION['cart_items'] as $key=> $value){ ?>
-                    <tr>
-                        <td><?php echo get_the_title($value['p_id']); ?></td>
-                        <td><?php echo get_the_post_thumbnail( $value['p_id'], 'thumbnail'); ?></td>
-                        <td><?php echo $value['p_qty']; ?></td>
-                    </tr>
-                <?php  }
-            }
-            ?>
-            </tbody>
-        </table>
 
-        <form>
+        <form action="<?php echo esc_url( admin_url('admin-post.php') ); ?>" method="post">
+
+            <input type="hidden" id="user_name" name="user_name" value="<?php echo $current_user->display_name;  ?>">
+
             <div class="form-row">
-              <textarea placeholder="Shipping address"id="shipping" name="shipping" cols="22" rows="3"></textarea>
+              <textarea placeholder="Shipping address" id="shipping_add" name="shipping_add" cols="22" rows="3" required></textarea>
               <span style="display:inline-block; width: 10px;"></span>
-              <textarea placeholder="Billing address" id="billing" name="billing" cols="22" rows="3"></textarea>
+              <textarea placeholder="Billing address" id="billing_add" name="billing_add" cols="22" rows="3" required></textarea>
             </div>
             <?php // echo 'User email: ' . $current_user->user_email . "\n";?>
 
-            <br>
-            <div class="form-group col-md-4">
+           <br>
+           Email
+           <input class="form-control" type="text" placeholder="<?php echo $current_user->user_email;  ?>" readonly>
+           <input type="hidden" name="user_email" id="user_email" value="<?php echo $current_user->user_email; ?>">
+
+           <br>
+           <div class="form-group col-md-4">
                 <label for="inputState">Payment options</label>
-                <select id="inputState" class="form-control" >
-                    <option selected>Select</option>
+                <select id="paymentMtd" name="paymentMtd" class="form-control" >
                     <option value="cod">COD</option>
                     <option value="card">Credit/Card</option>
                     <option value="netbank">Net banking</option>
                 </select>
             </div>
+            <br>
 
-
-            <div class="form-row">
-            <span style="margin-left: 320px;"><?php echo (count($_SESSION['cart_items'])); ?> ITEMS</span>
             <?php
             $total_price=0;
-            foreach($_SESSION['cart_items'] as $key=> $value){
-                   $total_price+=$value['p_price']*$value['p_qty'];
+            $total_quantity=0;
+            if(!empty($_SESSION['cart_items'])){
+                foreach($_SESSION['cart_items'] as $key=> $value){
+                       $total_price+=$value['p_price']*$value['p_qty'];
+                       $total_quantity+=$value['p_qty'];
+                }
             }
             ?>
-            <span style="margin-left: 75px;">Total Payable   Rs. <?php echo $total_price; ?></span>
 
-            <button type="submit" class="btn btn-primary mb-2" style="margin-left: 635px;">Submit Order</button>
+            <div class="form-row">
+                <input type="hidden" id="cart_item" name="cart_item" value="<?php echo $total_quantity; ?>">
+                <span style="margin-left: 320px;"><?php echo $total_quantity;?> - ITEMS</span>
+
+                <input type="hidden" name="total_price" id="total_price" value="<?php echo $total_price; ?>">
+                <span style="margin-left: 75px;" id="total_price" name="total_price">Total Payable   Rs. <?php echo $total_price; ?></span>
+
+                <input type="hidden" name="action" value="confirmation">
+
+                <button type="submit" name="submit" class="btn btn-primary mb-2" style="margin-left: 635px;">Submit Order</button>
+
             </div>
         </form>
+
 
     </div>
 

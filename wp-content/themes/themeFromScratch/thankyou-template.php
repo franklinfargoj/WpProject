a2c5b779bd10f2943850e6afbb429f850c2e66db
Franklin <franklin.fargoj@neosofttech.com>
2018-12-31 16:58:09 +0530

    modification in thank you template

--- wp-content/themes/themeFromScratch/thankyou-template.php
+++ wp-content/themes/themeFromScratch/thankyou-template.php
@@ -3,8 +3,21 @@
     get_header();
 ?>
 
-<h4>Order placed successfully.</h4>
-<br>
-<h2>Thank you for shopping with us.</h2>
+
+    <div class="row">
+    <div class="col-lg-2">
+    </div>
+
+
+    <div class="col-lg-8">
+    <h4 style="color: #1e7e34">Order placed successfully.</h4>
+    <br>
+    <h2 style="color: #1e7e34">Thank you for shopping with us.</h2>
+    </div>
+
+
+    <div class="col-lg-2">
+    </div>
+    </div>
 
 <?php get_footer(); ?>

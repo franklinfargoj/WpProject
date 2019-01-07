97f126e20ead97af20b02f9fa240ef76c23d8691
Franklin <franklin.fargoj@neosofttech.com>
2019-01-07 14:32:04 +0530

    wishlist template added shortcode

--- wp-content/themes/themeFromScratch/wishlist.php
+++ wp-content/themes/themeFromScratch/wishlist.php
@@ -2,45 +2,43 @@
 /*Template name: Wishlist page*/
 get_header();
 ?>
+
+
+<body>
+<div class="container">
     <div class="row">
-    <div class="col-lg-2">
-        <?php get_sidebar(); ?>
-    </div>
 
-    <div class="col-lg-8">
-        <h2> Wishlist Items</h2>
-        <table class="table">
-            <thead>
-            <tr>
-                <th>Product Name</th>
-                <th>Image</th>
-                <th>Price</th>
-                <th>Action</th>
-                <th>Action</th>
-            </tr>
-            </thead>
+        <div class="col-lg-2">
+            <?php get_sidebar(); ?>
+        </div>
 
-
+        <div class="col-lg-8">
             <?php
-            $wishlist = json_decode(get_user_meta(get_current_user_id(), 'user_wishlist', true));
-            foreach ($wishlist as $key => $value){
+            if ( have_posts() ) {
+                while ( have_posts() ) : the_post();
+                    ?>
+                    <div class="blog-post">
+                        <h2 class="blog-post-title"><?php the_title(); ?></h2>
+                        <?php
+                        if ( has_post_thumbnail() ) { // check if the post has a Post Thumbnail assigned to it.
+                            the_post_thumbnail( 'thumbnail' );
+                        }
+                        ?>
+                        <?php the_content(); ?>
+                    </div><!-- /.blog-post -->
+                    <?php
+                endwhile;
+            }
             ?>
 
-            <tbody>
-            <td><?php echo get_the_title( $value ); ?></td>
-            <td><?php echo get_the_post_thumbnail( $value, 'thumbnail' ); ?></td>
-            <td><?php echo get_post_meta($value, 'my_product_price_value_key', true); ?></td>
-            <td><a href="javascript:void(0);" class=" " data-value=$value><button>Add to cart</button></a></td>
-            <td><a href="javascript:void(0);" class="remove_wishlist" data-value=<?php echo $value ?> ><button>Remove</button></a></td>
-            <?php } ?>
-            </tbody>
+        </div>
 
-        </table>
-    </div>
-
-    <div class="col-lg-2">
-        <?php get_sidebar('right'); ?>
-    </div>
+        <div class="col-lg-2">
+            <?php get_sidebar('right'); ?>
+        </div>
 
     </div>
+</div>
+</body>
+
 <?php get_footer(); ?>

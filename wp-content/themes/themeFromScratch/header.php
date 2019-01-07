f55c89bcde4e99f6d17b103be1afc1c543c4341e
Franklin <franklin.fargoj@neosofttech.com>
2019-01-07 10:42:13 +0530

    Added header

--- wp-content/themes/themeFromScratch/header.php
+++ wp-content/themes/themeFromScratch/header.php
@@ -16,7 +16,7 @@
     <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
-    <?php wp_head(); ?>
+    <?php wp_head();?>
 </head>
 
   <nav class="navbar navbar-expand-sm bg-light">
@@ -32,7 +32,27 @@
       </div>
 
       <?php
-          $args = array(
+
+       //  print_r( site_url(). '/wp-content/plugins/wp_ecommerce/jsPage.js');die;
+       //  print_r(get_template_directory_uri() . '/jsPage.js');die;
+      // echo dirname( __FILE__ ) ;
+      // print_r(get_template_directory_uri().'/thankyou-template.php');die;
+//      print_r(get_home_url());die;
+//      print_r(get_author_posts_url( get_current_user_id() ));die;
+//      && get_nav_menu_locations()['primary']=='5'
+//      print_r(get_nav_menu_locations());die;
+//      echo admin_url();//die;
+//      echo get_template_directory_uri();
+//      echo get_stylesheet_uri();
+    //  echo site_url().'/login/';
+/*    $current_user =  wp_get_current_user();
+      $menuLocations = get_nav_menu_locations(); // Get our nav locations (set in our theme, usually functions.php)
+      echo"<pre>";
+      print_r($current_user);
+      die;*/
+      //print_r( get_user_meta(1, 'comment_shortcuts', true ));
+
+      $args = array(
               'theme_location' => 'primary',
               'container' => 'ul',
               'menu_class' => 'nav nav-tabs menu ml-auto',

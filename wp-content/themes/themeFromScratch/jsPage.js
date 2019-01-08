91b7fd47b7c0bbf7c94b4c10110014bf78875f1c
Franklin <franklin.fargoj@neosofttech.com>
2019-01-08 10:44:36 +0530

    Code removed and moved in plugin js file

--- wp-content/themes/themeFromScratch/jsPage.js
+++ wp-content/themes/themeFromScratch/jsPage.js
@@ -25,113 +25,6 @@ jQuery(document).ready(function() {
         return true;
     }
 
-    $('.add-to-cart').click(function () {
-        var product_id = $(this).attr('data-value');
-        var price = $('#prod_' + product_id).text();
-        var quantity =  $('#txtNumber' + product_id).val();
-        jQuery.ajax({
-            type : "post",
-            dataType : "json",
-            url : ajax_params.ajax_url,
-            data : {action: "add_to_cart", product_id : product_id, price: price,quantity: quantity },
-            success : function( response ) {
-                console.log(response);
-                $(".header_cart").html(response.qty_cart);
-                $('.add_cart_'+response.product.p_id).html('<span style="color:#FE980F">' + "Added to the cart!" + '</span>');
-            }
-        })
-    });
-
-    $('.addto-wishlist').click(function () {
-        var product_id = $(this).attr('data-value');
-        var custId = $('#custId').val();
-        if(custId != 0){
-        jQuery.ajax({
-            type : "post",
-            dataType : "json",
-            url : ajax_params.ajax_url,
-            data : {action: "add_to_wishlist", product_id : product_id}
-        })
-        }else{
-            window.location.replace("http://10.0.11.43/WpProject/login");
-        }
-    });
-
-    $('.remove_wishlist').click(function () {
-        var product_id = $(this).attr('data-value');
-        jQuery.ajax({
-            type : "post",
-            dataType : "json",
-            url : ajax_params.ajax_url,
-            data : {action: "remove_from_wishlist", product_id : product_id}
-        })
-    });
-
-    $('.cart_qty_up').click(function () {
-        var product_id = $(this).attr('data-value');
-        var price = $(this).attr('data-value1');
-        var quantity = 1;
-
-        jQuery.ajax({
-            type : "post",
-            datatype :"json",
-            url: ajax_params.ajax_url,
-            data : { action : "cart_qty_increase",
-                product_id : product_id,
-                quantity: quantity,
-                price: price
-            },
-            success: function (response) {
-                var x = JSON.parse(response);
-                var prodt_price = (x.product.p_price)*x.product.p_qty;
-
-                $(".cart_qty_"+product_id).html(x.product.p_qty);
-                $(".per_product_price"+product_id).html(prodt_price);
-                $("#final_amount").html(x.total);
-            }
-        })
-    });
-
-    $('.remove_from_cart').click(function () {
-        var product_id = $(this).attr('data-value');
-        jQuery.ajax({
-            type : "post",
-            dataType : "json",
-            url : ajax_params.ajax_url,
-            data : {action: "delete_from_cart", product_id : product_id },
-            success : function( response ) {
-                //console.log(response.qty);
-
-                $(".cart_list_"+product_id).fadeOut();
-                $("#final_amount").html(response.total);
-
-                $(".header_cart").html(response.cart);
-
-                if(response.total == 0){
-                $("#checkout").fadeOut();
-                }
-            }
-        })
-    });
-
-    $('.cart_qty_down').click(function () {
-        var product_id = $(this).attr('data-value');
-        jQuery.ajax({
-            type: "post",
-            datatype : "json",
-            url: ajax_params.ajax_url,
-            data : { action: "cart_qty_decrease", product_id : product_id},
-            success:function (response) {
-                var x = JSON.parse(response);
-                console.log(x.total);
-                var p_price = x.sub_total.p_price*x.sub_total.p_qty;
-                $("#per_product_price"+product_id).html(p_price);
-                $(".cart_qty_"+product_id).html(x.sub_total.p_qty);
-                $("#final_amount").html(x.total);
-            }
-        })
-    });
-
     //redirects on click of Proceed to checkout
     $('#checkout').click(function () {
         window.location='./checkout/'
@@ -145,9 +38,7 @@ jQuery(document).ready(function() {
     $('#confirmation').click(function () {
         window.location='./confirmation/'
     });
-
-
-
+    
     $('#loginFormoId').submit(function() {
         var username = $( ".custom_username" ).val();
         var password = $( ".custom_password" ).val();
